8c3e1a300b7587bc2416ffa8232449395a345f19
Aldo Miralles <alditomiralles3@gmail.com>
2025-02-16 23:18:39 -0500

    adding p

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,7 @@ function App() {
       <div className="card">
         
       </div>
+      <p>sjsj</p>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
